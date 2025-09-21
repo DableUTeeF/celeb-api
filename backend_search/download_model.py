2f022172e60ca0b2ca4afbea3038e060b69865eb
DableUTeeF <palm22180@gmail.com>
2025-09-21 22:01:25 +0700

    Added db

--- backend_search/download_model.py
+++ backend_search/download_model.py
@@ -1,4 +1,66 @@
 from insightface.app import FaceAnalysis
-app = FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])  # Use 'CUDAExecutionProvider' for GPU
-# app.prepare(ctx_id=0)  # ctx_id=-1 for CPU, 0 for GPU
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Queue(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+
+if __name__ == '__main__':
+    app = FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])  # Use 'CUDAExecutionProvider' for GPU
+    # app.prepare(ctx_id=0)  # ctx_id=-1 for CPU, 0 for GPU
+
+    engine = create_engine("postgresql+psycopg2://postgres:postgres@db:5432/postgres", echo=True)
+
+    with Session(engine) as session:
+        spongebob = User(
+            name="spongebob",
+            fullname="Spongebob Squarepants",
+            addresses=[Address(email_address="spongebob@sqlalchemy.org")],
+        )
+        sandy = User(
+            name="sandy",
+            fullname="Sandy Cheeks",
+            addresses=[
+                Address(email_address="sandy@sqlalchemy.org"),
+                Address(email_address="sandy@squirrelpower.org"),
+            ],
+        )
+        patrick = User(name="patrick", fullname="Patrick Star")
+        session.add_all([spongebob, sandy, patrick])
+        session.commit()
+
 
